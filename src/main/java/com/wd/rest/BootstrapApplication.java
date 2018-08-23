eecd08ffbc1822eee1a33dc19e5ebe9028e74fb2
gavin <gaokuo_77@163.com>
2018-08-23 16:33:16 +0800

    引入swagger-spring-boot-starter

--- src/main/java/com/wd/rest/BootstrapApplication.java
+++ src/main/java/com/wd/rest/BootstrapApplication.java
@@ -1,9 +1,11 @@
 package com.wd.rest;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@EnableSwagger2Doc
 @SpringBootApplication
 @ServletComponentScan
 public class BootstrapApplication {
